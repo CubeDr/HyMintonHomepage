5ad537b1ea1eba57765a81094b0248c80287bda3
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-11-13 22:57:27 +0900

    nav, 공지 페이지 작성

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <header>
+      <HyHeader class="unSelectable"/>
+    </header>
+    <nav>
+      <HyNav class="unSelectable" :page="page" @click="page = $event" :selectColor="background"/>
+    </nav>
+
+    <component is="NoticePage"/>
+  </div>
+</template>
+
+<script>
+import HyHeader from "./components/HyHeader";
+import HyNav from "./components/HyNav";
+import NoticePage from "./pages/NoticePage";
+
+export default {
+  components: {HyNav, HyHeader, NoticePage},
+  data () {
+    return {
+      page: '공지사항',
+      background: '#F0F8FF'
+    }
+  }
+}
+</script>
+
+<style>
+
+#app {
+  width: 100%;
+  min-width: 700px;
+  max-width: 1100px;
+  margin: auto;
+}
+
+*.unSelectable {
+  -moz-user-select: -moz-none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+
+  /*
+    Introduced in IE 10.
+    See http://ie.microsoft.com/testdrive/HTML5/msUserSelect/
+  */
+  -ms-user-select: none;
+  user-select: none;
+}
+
+
+
+</style>
---
+++ src/components/HyNav.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="HyNav">
+      <div id="navHolder">
+        <HyNavButton class="navButton"
+                     :source="btn1Source"
+                     title="공지사항"
+                     page="notice"
+                     :current="page"
+                     :selectColor="selectColor"
+                     @navBtnClick="noticeClick($event)"/>
+        <HyNavButton class="navButton"
+                     :source="btn1Source"
+                     title="행사"
+                     page="event"
+                     :current="page"
+                     :selectColor="selectColor"
+                     @navBtnClick="noticeClick($event)"/>
+        <HyNavButton class="navButton"
+                     :source="btn1Source"
+                     title="회비"
+                     page="fee"
+                     :current="page"
+                     :selectColor="selectColor"
+                     @navBtnClick="noticeClick($event)"/>
+        <HyNavButton class="navButton"
+                     :source="btn1Source"
+                     title="셔틀콕"
+                     page="shuttle"
+                     :current="page"
+                     :selectColor="selectColor"
+                     @navBtnClick="noticeClick($event)"/>
+      </div>
+    </div>
+</template>
+
+<script>
+    import HyNavButton from "./HyNavButton";
+    export default {
+        name: "HyTab",
+        props: ['page', 'selectColor'],
+        components: {HyNavButton},
+        data() {
+          return {
+            btn1Source: require("../assets/logo_white.png"),
+          }
+        },
+        methods: {
+          noticeClick(what) {
+            this.$emit('click', what)
+          }
+        }
+    }
+</script>
+
+<style scoped>
+#HyNav {
+  background: #47b8e0;
+  width: 100%;
+}
+
+#navHolder {
+  display: flex;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+}
+
+.navButton {
+  margin: 5px 20px 0px 20px;
+}
+</style>
