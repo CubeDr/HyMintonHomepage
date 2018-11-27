e6682450e8798367f6719d6b39b60c0d78dbba5c
이현규 <qnd4321@naver.com>
2018-11-27 18:02:55 +0900

    Notice 쿼리문 작성

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -5,17 +5,11 @@ var dbconfig = require('../config/dbconfig.js');
 var connection = mysql.createConnection(dbconfig);
 var path = require('path')
 
-// configuration
-router.get('/',function(req, res, next) {
-  res.render('db', { title: 'Express' })
-  console.log('db test')
-});
-
+// notice - list
 router.get('/notice/list', function(req, res, next){
-  console.log('test page in !!')
   connection.query("SELECT NID, title, ndate, hits \
                     FROM Notice \
-                    ORDER BY ndate DESC, NID ASC limit 5;", function(err, rows, fields){
+                    ORDER BY ndate DESC, NID ASC limit 20;", function(err, rows, fields){
     if(err){
       console.log("/notice/list error");
       throw err;
@@ -24,9 +18,21 @@ router.get('/notice/list', function(req, res, next){
   })
 })
 
+// notice - by ID
 router.get('/notice/:id', function(req, res, next){
-
+  var id = parseInt(req.params.id, 10)
+  connection.query("SELECT n.NID, n.title, u.fname, u.lname,\
+                           n.ndate, n.hits, n.ncontent \
+                    FROM Notice n, User u WHERE n.ID=u.id \
+                    ORDER BY n.ndate DESC, n.NID ASC limit 1;", function(err, rows, fields){
+    if(err){
+      console.log("해당 게시물이 삭제되거나 변경되었습니다.");
+      throw err;
+    }
+    res.send(rows)    
+  })
 })
+
 
 router.get('/s', function(req, res, next) {
   connection.query("SELECT * FROM User WHERE ID='2014004120';",function(err, rows, fields){
@@ -53,5 +59,6 @@ router.get('/i', function(req, res, next) {
     res.send(200, 'success');
   });
 });
+
 
 module.exports = router;
