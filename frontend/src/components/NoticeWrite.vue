28ddceb6ad096e7a77a4d2771bd7fd33fb0601a8
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-03 13:49:57 +0900

    공지 작성 페이지 레이아웃

--- frontend/src/components/NoticeWrite.vue
+++ frontend/src/components/NoticeWrite.vue
@@ -1,10 +1,9 @@
 <template>
-    <div id="noticeWrite">
+    <div>
       <form>
-        <label id="lbTitle">제목</label>
-        <input id="ipTitle" type="text" title="제목"/>
+        <input id="ipTitle" type="text" placeholder="제목"/>
         <textarea id="taContent" title="내용"></textarea>
-        <div>
+        <div id="btns">
           <button @click.prevent="cancel()">취소</button>
           <button @click.prevent="submit()">작성</button>
         </div>
@@ -27,17 +26,33 @@
 </script>
 
 <style scoped>
-#lbTitle {
-  width: 5%;
-  font-weight: bold;
+
+form {
+  width: 90%;
+  margin: auto;
+}
+
+#ipTitle, #taContent {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 5px;
 }
 
 #ipTitle {
-  margin: 0;
+  font-size: 30px;
 }
 
-#noticeWrite {
-  width: 50%;
-  margin: auto;
+#taContent {
+  height: 500px;
+  margin-top: 10px;
+  resize: none;
+  font-size: 20px;
 }
+
+#btns {
+  text-align: right;
+}
+
 </style>
