4e56fc9ec9ae4712f8767b5ec46372ab20714a56
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-04 19:24:34 +0900

    vuetify 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
 import axios from 'axios'
 
+import 'vuetify/dist/vuetify.min.css'
+
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.use(Vuetify);
 
 export const eventBus = new Vue();
 
