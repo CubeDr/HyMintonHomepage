2657d40c3ba1d92e88bda6a3e951547172f7e939
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-04 20:15:36 +0900

    로그인페이지 양식 완성

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,15 +1,72 @@
 <template>
     <div>
-      Login
+      <v-form id='login' ref="form" v-model="valid">
+        <v-text-field
+          v-model="id"
+          :rules="idRules"
+          label="학번"
+          type="text"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="pw"
+          :rules="pwRules"
+          label="비밀번호"
+          type="password"
+          required
+        ></v-text-field>
+
+        <v-btn
+          :disabled="!valid"
+          @click="submit"
+        >
+          submit
+        </v-btn>
+        <v-btn @click="clear">clear</v-btn>
+        <v-btn id="cancelButton" @click="back">취소</v-btn>
+      </v-form>
     </div>
 </template>
 
 <script>
+    import router from '../router';
+
     export default {
-        name: "LoginPage"
+        name: "LoginPage",
+        data() {
+          return {
+            valid: false,
+            id: '',
+            pw: '',
+            idRules: [(id) => id.length > 8 || '8자 이상 입력해야 합니다.'],
+            pwRules: [(pw) => pw.length > 8 || '8자 이상 입력해야 합니다.']
+          }
+        },
+        methods: {
+          submit() {
+            console.log('login request');
+          },
+          clear() {
+            this.id = '';
+            this.pw = '';
+          },
+          back() {
+            router.push({name: 'IndexPage'});
+          }
+        }
     }
 </script>
 
 <style scoped>
-
+#login {
+  width: 50%;
+  margin: 50px auto;
+  padding: 10px 30px 10px 30px;
+  box-shadow: 10px 10px 20px 10px lightgray;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+#cancelButton {
+  float: right;
+}
 </style>
