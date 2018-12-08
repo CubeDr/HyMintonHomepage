343e62d26b358aa836ecb422719f94c2fa873511
이현규 <qnd4321@naver.com>
2018-12-09 00:34:54 +0900

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.user(bodyParser().json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
