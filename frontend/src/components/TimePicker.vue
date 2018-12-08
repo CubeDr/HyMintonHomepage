3beec730b6399e1b176bc4d6e330bd387cda0aeb
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-08 12:10:56 +0900

    일정 추가 카드의 시작시간/종료시간 한 줄에 출력

--- frontend/src/components/TimePicker.vue
+++ frontend/src/components/TimePicker.vue
@@ -1,31 +1,86 @@
 <template>
-    <div style="width: 100%; text-align:center">
-      <v-dialog
-        v-if="selectDate"
-        ref="dialog"
-        v-model="dateDialog"
-        :return-value.sync="date"
-        persistent
-        lazy
-        full-width
-        width="290px"
-      >
-        <v-text-field
-          slot="activator"
-          v-model="date"
-          label="Picker in dialog"
-          prepend-icon="event"
-          readonly
-        ></v-text-field>
-        <v-date-picker v-model="date" no-title scrollable color="blue">
-          <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="dateDialog = false">Cancel</v-btn>
-          <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-        </v-date-picker>
-      </v-dialog>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <!-- 날짜 선택 -->
+      <v-flex xs12>
+        <v-dialog
+          v-if="selectDate"
+          ref="dateDialog"
+          v-model="dateDialog"
+          :return-value.sync="date"
+          persistent
+          lazy
+          full-width
+          width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="date"
+            label="Picker in dialog"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title scrollable color="blue">
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="dateDialog = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.dateDialog.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </v-flex>
 
-      <v-time-picker :landscape="true" :small="true" :full-width="true"></v-time-picker>
-    </div>
+      <!-- 시작 시간 선택 -->
+      <v-flex xs6>
+        <v-dialog
+          ref="startDialog"
+          v-model="startDialog"
+          :return-value.sync="start"
+          persistent
+          lazy
+          full-width
+          width="450px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="start"
+            label="시작시간"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-time-picker :landscape="true" :full-width="true" color="blue" v-model="start">
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="startDialog = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.startDialog.save(start)">OK</v-btn>
+          </v-time-picker>
+        </v-dialog>
+      </v-flex>
+
+      <!-- 종료 시간 선택 -->
+      <v-flex xs6>
+        <v-dialog
+          ref="endDialog"
+          v-model="endDialog"
+          :return-value.sync="end"
+          persistent
+          lazy
+          full-width
+          width="450px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="end"
+            label="종료시간"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-time-picker :landscape="true" :full-width="true" color="blue" v-model="end">
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="endDialog = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.endDialog.save(end)">OK</v-btn>
+          </v-time-picker>
+        </v-dialog>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -35,8 +90,12 @@
       data() {
         return {
           time: this.value,
-          date: new Date().toISOString().substr(0, 10), // "2012-12-08"
-          dateDialog: false
+          date: new Date().toISOString().substr(0, 10), // "2012-12-08",
+          start: '16:00',
+          end: '19:00',
+          dateDialog: false,
+          startDialog: false,
+          endDialog: false
         }
       },
       methods: {
