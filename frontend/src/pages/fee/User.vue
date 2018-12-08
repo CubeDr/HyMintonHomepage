a52dead13fd2c0725b02fa7b85eb48beec65995e
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-09 06:03:49 +0900

    본인 회비 페이지 작업중

--- frontend/src/pages/fee/User.vue
+++ frontend/src/pages/fee/User.vue
@@ -42,48 +42,14 @@
         loadPaymentInfo() {
           // load from url
           this.$http.get('http://115.140.236.238:14707/db/payment/' + this.$route.params.id).then((res) => {
-            let d = res.data.map((data) => {
+            this.payments = res.data.map((data) => {
               return {
                 name: data.fid.slice(0,2) + "년 " + data.fid.slice(2,4) + "월 회비",
                 price: data.price,
                 date: Time.fromFormatString(data.date)
               }
-            })
-            console.log(d);
+            });
           });
-          this.payments = [
-            {
-              value: true,
-              name: '12월 회비',
-              price: '10,000원',
-              date: '2018년 12월 11일'
-            },{
-              value: true,
-              name: '11월 회비',
-              price: '10,000원',
-              date: '2018년 12월 10일'
-            },{
-              value: true,
-              name: '10월 회비',
-              price: '10,000원',
-              date: '2018년 12월 12일'
-            },{
-              value: true,
-              name: '9월 회비',
-              price: '10,000원',
-              date: '2018년 12월 13일'
-            },{
-              value: true,
-              name: '8월 회비',
-              price: '10,000원',
-              date: '2018년 12월 09일'
-            },{
-              value: true,
-              name: '7월 회비',
-              price: '10,000원',
-              date: '2018년 12월 08일'
-            },
-          ];
         }
       }
     }
