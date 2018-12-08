a074acd00d21c7c7c4d89874eaad621a56ceb182
HyunI Kim <spaceship00@hanyang.ac.kr>
2018-12-08 21:16:45 +0900

    자신 회비 조회 표

--- frontend/src/pages/fee/User.vue
+++ frontend/src/pages/fee/User.vue
@@ -1,15 +1,75 @@
 <template>
-    <div>
-
-    </div>
+  <v-container grid-list-md>
+    <v-layout row wrap align-center justify-center>
+      <v-data-table
+        :headers="headers"
+        :items="payments"
+        class="elevation-1"
+      >
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.price }}</td>
+          <td class="text-xs-right">{{ props.item.date }}</td>
+        </template>
+      </v-data-table>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
     export default {
-        name: "User"
+      name: "User",
+      data () {
+        return {
+          headers: [
+            {
+              text: '회비명',
+              align: 'left',
+              value: 'name'
+            },
+            { text: '금액', value: 'price' },
+            { text: '납부일', value: 'date' }
+          ],
+          payments: [
+            {
+              value: true,
+              name: '12월 회비',
+              price: '10,000원',
+              date: '2018년 12월 11일'
+            },{
+              value: true,
+              name: '11월 회비',
+              price: '10,000원',
+              date: '2018년 12월 10일'
+            },{
+              value: true,
+              name: '10월 회비',
+              price: '10,000원',
+              date: '2018년 12월 12일'
+            },{
+              value: true,
+              name: '9월 회비',
+              price: '10,000원',
+              date: '2018년 12월 13일'
+            },{
+              value: true,
+              name: '8월 회비',
+              price: '10,000원',
+              date: '2018년 12월 09일'
+            },{
+              value: true,
+              name: '7월 회비',
+              price: '10,000원',
+              date: '2018년 12월 08일'
+            },
+          ]
+        }
+      }
     }
 </script>
 
 <style scoped>
-
+.center {
+  text-align: center;
+}
 </style>
